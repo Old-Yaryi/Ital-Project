912b0faea4d9d72ad7fad9c4727d950ce42ee9b2
Old-Yaryi <kinomir-v@yandex.ru>
2022-04-08 17:33:30 +0300

    'servises_begin'

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
   $('.slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
   });
 
 });
